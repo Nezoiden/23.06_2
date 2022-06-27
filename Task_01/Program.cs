29ef8b5cf3250121374550f360e4004d9592ea60
Sergey <xo4ywka@yandex.ru>
2022-06-27 21:45:22 +0300

    Задача 9

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -7,12 +7,12 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 
-Console.WriteLine ("Введите первое число:");
+Console.WriteLine("Введите первое число:");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Введите второе число:");
+Console.WriteLine("Введите второе число:");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 == num2 * num2)
-    {
-        Console.WriteLine ("Первое число является квадратом второго!");
-    }
-else Console.WriteLine ("Первое число не является квадратом второго!");
+{
+    Console.WriteLine("Первое число является квадратом второго!");
+}
+else Console.WriteLine("Первое число не является квадратом второго!");
