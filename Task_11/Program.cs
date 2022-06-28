ce2fdfedfc4111c2617732d9bd3f9f7faeae101f
Sergey <xo4ywka@yandex.ru>
2022-06-28 16:50:44 +0300

    добавил 12 и 14

---
+++ Task_11/Program.cs
@@ -0,0 +1,17 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+
+int rndNum = new Random().Next(101, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {rndNum}");
+
+int firstDigit = rndNum / 100;
+int lastDigit = rndNum % 10;
+
+//Console.WriteLine($"{firstDigit}{lastDigit}");
+Console.WriteLine(firstDigit * 10 + lastDigit);
---
+++ Task_12/Program.cs
@@ -0,0 +1,24 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+void Kratno(int in_a, int in_b)
+{
+    int ostatok = in_a % in_b;
+    //Console.Write($"Остаток равен {ostatok} ");
+    if (ostatok == 0)
+        Console.Write($"кратно");
+    else
+        Console.Write($"не кратно, остаток {ostatok}");
+}
+
+Kratno(a, b);
