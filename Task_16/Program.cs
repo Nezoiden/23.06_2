e12859670903103e18a2c5b8c83b232ea53dd53f
Sergey <xo4ywka@yandex.ru>
2022-07-04 17:20:17 +0300

    Третий семинар

--- Task_16/Program.cs
+++ Task_16/Program.cs
@@ -5,3 +5,21 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+Kratno(a, b);
+// Console.WriteLine("Введите первое число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 == num2 * num2)
+// {
+//     Console.WriteLine("Первое число является квадратом второго!");
+// }
+// else Console.WriteLine("Первое число не является квадратом второго!");
---
+++ Task_22/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Console.Clear();
+// Console.WriteLine("Введите  число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= num)
+// {
+//     Console.WriteLine($"{i}\t|    {i * i}");
+//     i++;
+// }
+
+Console.Clear();
+Console.WriteLine("Введите  число: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++) 
+{
+    Console.WriteLine($"Квадрат числа {i} = {i*i,4}");
+}
