71450f9fc80416035a6fba8c65ae1386eb044a37
Sergey <xo4ywka@yandex.ru>
2022-07-08 17:35:00 +0300

    четвёртый семинар

---
+++ Task_26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Введите число:  ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+while (num != 0)
+{
+    num /= 10;
+    count++;
+    // Console.WriteLine(num%10);
+}
+Console.WriteLine($"Количество цифр в числе = {count}");
---
+++ Task_28/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+// Console.Clear();
+// Console.WriteLine("Введите число:");
+// int num = int.Parse(Console.ReadLine());
+
+// void Proizvedenie(int number)
+// {
+//     int pro = 1;
+
+//     for (int i = 1; i <= number; i++)
+//     {
+//         // sum = sum * i;
+//         pro *= i;
+//     }
+
+//     Console.WriteLine(pro);
+// }
+
+// Proizvedenie(num);
+
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+int res = 1;
+int start = 1;
+
+while (start <= num)
+{
+    checked
+    {
+        res *= start;
+        start++;
+    }
+}
+
+Console.WriteLine($"Произведение чисел от одного до {start - 1} равно {res}");
